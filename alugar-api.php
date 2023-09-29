1004c2161332398b8597919a81cc2cead0fcc632
P4uleira <pricardoporfirio2001@gmail.com>
2023-09-29 19:43:46 -0300

    Corrigido css para computador

--- alugar-api.php
+++ alugar-api.php
@@ -41,11 +41,12 @@ function alugarfilme($id, $cat) {
     foreach ($filmes->results as $filme) {
         if ($filme->id == $id) {      
             
-          echo "<div class=\"box-filme\">";          
+          echo "<div class=\"box-filme-alugar\">";          
               $releaseYear = new DateTimeImmutable($filme->release_date);
-              echo "<div class=\"box-poster\">";
-              echo "<img class=\"img\" src='https://image.tmdb.org/t/p/w500" . $filme->poster_path . "'alt='Poster do Filme'>";
-              echo "<h6 class=\"box-informacoes\"><em>" . $filme->title . "</em><br><strong>" . $releaseYear->format('Y') . "</strong></h6>";
+              echo "<div class=\"box-poster-alugar\">";
+              echo "<img class=\"img-alugar\" src='https://image.tmdb.org/t/p/w500" . $filme->poster_path . "'alt='Poster do Filme'>";
+              echo "<h6 class=\"box-informacoes-alugar\"><em>" . $filme->title . "</em><br><strong>" . $releaseYear->format('Y') . "</strong></h6>";
+              echo "<p class=\"box-sinopse-alugar\"><em>" .$filme -> overview . "</p>";
               echo "</div>";
               echo "</br>";
           
